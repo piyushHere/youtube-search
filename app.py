942f87269b20e8aeeb98e3d5634100afe9d47025
PiyushHere <piyushs948@gmail.com>
2022-10-08 19:12:39 +0530

    added youtube client and cronjob

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import time
+from clients.youtube import Youtube
  
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+youtube_client = Youtube()
 
 class Youtube(db.Model):
     __tablename__ = 'youtube'
@@ -18,9 +21,22 @@ class Youtube(db.Model):
     thumbnail_url = db.Column(db.String())
     publishing_datetime_ts = db.Column(db.DateTime(True), server_default=db.text("now()"))
 
+@app.cli.command()
+def scheduled():
+    """Run scheduled job."""
+    print('Importing feeds...')
+    time.sleep(5)
+    print('Users:')
+    print('Done!')
+
+def fetch_youtube():
+    response = youtube_client.get_search_response()
+    return response
+
 # main driver function
 if __name__ == '__main__':
  
     # run() method of Flask class runs the application
     # on the local development server.
+    fetch_youtube()
     app.run(port=8000, debug=True)
--- clients/youtube.py
+++ clients/youtube.py
@@ -1,3 +1,4 @@
+from urllib.parse import urljoin
 import requests
 from os import environ, path
 from dotenv import load_dotenv
@@ -6,7 +7,7 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
-API_HOST = environ.get('https://youtube.googleapis.com')
+API_HOST = environ.get('YOUTUBE_HOST')
 API_KEY = environ.get('YOUTUBE_DATA_V3_KEY')
 
 class Youtube():
@@ -14,10 +15,15 @@ class Youtube():
         self.api_host = API_HOST
         self.search_api = "youtube/v3/search"
         self.api_key = API_KEY
-    
 
     def get_search_response(self):
-        response = requests.get()
-        if 500 <= response.status_code < 600:
-            error_msg = 'Youtube api not responding'
+        url = urljoin(
+            self.api_host, self.search_api
+        )
+        params = {
+            'part': 'snippet',
+            'key': self.api_key
+        }
+        response = requests.get(url, params)
+        return response.json()
             
