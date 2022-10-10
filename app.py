1c715114df8df0044838f65ade557c34526da91e
PiyushHere <piyushs948@gmail.com>
2022-10-10 22:43:55 +0530

    moved table to models

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 import config
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from clients.youtube import Youtube
+youtube_client = Youtube()
 from sqlalchemy import desc
 import json
  
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
-db = SQLAlchemy(app)
+from models import db, Youtube
 migrate = Migrate(app, db)
-youtube_client = Youtube()
 
-class Youtube(db.Model):
-    __tablename__ = 'youtube'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String())
-    desciption = db.Column(db.String())
-    thumbnail_url = db.Column(db.String())
-    publishing_datetime_ts = db.Column(db.DateTime(True), server_default=db.text("now()"))
 
 @app.cli.command()
 def scheduled():
---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy(app)
+
+
+class Youtube(db.Model):
+    __tablename__ = 'youtube'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    desciption = db.Column(db.String())
+    thumbnail_url = db.Column(db.String())
+    publishing_datetime_ts = db.Column(db.DateTime(True), server_default=db.text("now()"))
