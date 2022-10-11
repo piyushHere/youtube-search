3670d32648b2463e2830db2ff4778a1e24e5f03c
PiyushHere <piyushs948@gmail.com>
2022-10-12 00:18:58 +0530

    added methods in youtube service

--- services/youtube.py
+++ services/youtube.py
@@ -1,10 +1,12 @@
 from dao.youtube import YoutubeDAO
+from clients.youtube import YoutubeClient
 from flask import request
 
 
 class YoutubeService(object):
     def __init__(self) -> None:
         self.youtube_dao = YoutubeDAO()
+        self.youtube_client = YoutubeClient()
     
     def get_youtube_data(self):
         page = request.args.get('page', 1, type=int)
@@ -27,4 +29,21 @@ class YoutubeService(object):
                 "description": video.desciption
             })
         return response
+    
+    def fetch_youtube(self):
+        response = self.youtube_client.get_search_response()
+        response = self.get_response_items_dict(response)
+        self.youtube_dao.insert(response)
+
+    def get_response_items_dict(self, response):
+        items = response["items"]
+        response_list = []
+        for item in items:
+            response_list.append({
+                "title": item["snippet"]["title"],
+                "desciption": item["snippet"]["description"],
+                "publishing_datetime_ts": item["snippet"]["publishTime"],
+                "thumbnail_url": item["snippet"]["thumbnails"]["default"]["url"]
+            })
+        return response_list
 
